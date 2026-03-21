99733397a304072cdf71b78ed76bd657a97fdbe3
Veronica Falcon <veritofalconlea@gmail.com>
2026-03-21 14:57:15 +0000

    Included all files

--- vehicle/movement.py
+++ vehicle/movement.py
@@ -6,3 +6,25 @@ def backward():
 
 def stop():
     print("Stopping")
+
+
+from machine import Pin
+
+#placeholders 
+in1 = none #cambiar 
+in2 = none #cambiar por pins later 
+
+def forward():
+    in1.value(1)
+    in2.value(0)
+    print("Moving forward")
+
+def backward():
+    in1.value(0)
+    in2.value(1)
+    print("Moving backward")
+
+def stop():
+    in1.value(0)
+    in2.value(0)
+    print("Stopping")
